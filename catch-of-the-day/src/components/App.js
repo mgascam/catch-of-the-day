051bfc04aa774566e68a9d8211f6bacf029bcafd
Miguel Gasca <mgasca@theworkshop.com>
2018-08-27 19:21:46 +0200

    13 - understanding state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,27 @@ import Inventory from './Inventory';
 import Order from './Order';
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = (fish) => {
+        console.log("adding a fish!");
+        // 1. Take a copy of existing state
+        const fishes = {...this.state.fishes};
+        // 2. Add new fish to fishes variable
+        fishes[`fish${Date.now()}`] = fish;
+        // 3. Set the new fishes object to state
+        this.setState({ fishes });
+    };
     render() {
        return (
            <div className="catch-of-the-day">
             <div className="menu">
                 <Header tagline="Fresh Seafood Market" />
             </div>
-               <Inventory />
                <Order />
+               <Inventory addFish={this.addFish} />
            </div>
        );
     }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -11,7 +11,8 @@ class StorePicker extends React.Component {
     };
     render() {
         return (
-          <form className="store-selector" onSubmit={this.goToStore}>
+          <form className="store-selector"
+                onSubmit={this.goToStore}>
             <h2>Please Enter A Store</h2>
             <input
                 type="text"
